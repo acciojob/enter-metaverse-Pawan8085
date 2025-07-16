de0c589d18c4fc594175ca02c0948f752497026e
Pawan Kumar <101393436+Pawan8085@users.noreply.github.com>
2025-07-16 11:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById('enterBtn').addEventListener('click', () => {
 	let p = document.getElementById('status');
 	document.body.removeChild(p);
 	let h1 = document.createElement('h1');
+	h1.setAttribute('id', 'status')
 	h1.innerText = 'Entered Metaverse';
 	document.body.insertBefore(h1, document.body.firstChild);
 })
