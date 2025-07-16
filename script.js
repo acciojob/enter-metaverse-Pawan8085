2b065441f02d50a3a3adffcd6c68abc3f2ac25ec
Pawan Kumar <101393436+Pawan8085@users.noreply.github.com>
2025-07-16 11:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', () => {
+	let p = document.getElementById('status');
+	document.body.removeChild(p);
+	let h1 = document.createElement('h1');
+	h1.innerText = 'Entered Metaverse';
+	document.body.insertBefore(h1, document.body.firstChild);
+})
